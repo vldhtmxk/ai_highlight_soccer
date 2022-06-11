8927ca2518684539c0d54cd9e4df9d2fe3cae9a1
SongJwans <104306841+SongJwans@users.noreply.github.com>
2022-06-11 22:25:35 +0900

    Add files via upload

---
+++ ai_highlight_soccer/config.py
@@ -0,0 +1,45 @@
+IMAGE_CONFIG = {
+    "resize_origin":
+        {
+            "standard_width": 512,
+            "standard_height": 512
+        },
+    "gradient":
+        {
+            "kernel_size_row": 3,
+            "kernel_size_col": 3
+        },
+    "threshold":
+        {
+            "mode": "gaussian",
+            "block_size": 15,
+            "subtract_val": 21
+
+        },
+    "remove_line":
+        {
+            "threshold": 200,
+            "min_line_length": 53,
+            "max_line_gap": 200
+        },
+    "close":
+        {
+            "kernel_size_row": 25,
+            "kernel_size_col": 10
+        },
+    "contour":
+        {
+            "min_width": 100,
+            "min_height": 15,
+            "section_x": 100,
+            "section_y": 50,
+            "padding": 2
+        }
+}
+
+RECO_CONFIG = {
+    "tesseract": 'C:/Program Files/Tesseract-OCR/tesseract.exe',
+    "custom_oem_psm_config": '--oem 3 --psm 6',
+    "custom_config": '--psm 1 -c preserve_interword_spaces=1',
+    "lang": 'eng'
+}
